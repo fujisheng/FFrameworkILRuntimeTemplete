3f7a8f48eab57365d70cfbd688d54b64706ac7cc
fujisheng <fujisheng@foxmail.com>
2020-11-20 19:38:41 +0800

    save

---
+++ Assets/Scripts/Hotfix/HomeView.cs
@@ -0,0 +1,20 @@
+﻿using Framework.IL.Hotfix.Module.UI;
+using UnityEngine.UI;
+
+namespace Game.Hotfix
+{
+    class HomeView : View
+    {
+        public override void Init()
+        {
+            base.Init();
+            resourceLoader.Get<Text>("111111111");
+        }
+
+        [BindProperty("Name")]
+        void OnChangedName(string oldValue, string newValue)
+        {
+            string a = newValue;
+        }
+    }
+}
