3a72650c8273b34319c14b15e2c7abe6cec11606
fujisheng <fujisheng@foxmail.com>
2020-11-29 03:40:44 +0800

    添加编辑器下直接load dll

--- Assets/Scripts/Hotfix/HomeViewModel.cs
+++ Assets/Scripts/Hotfix/HomeViewModel.cs
@@ -1,17 +1,20 @@
 ﻿using Framework.IL.Hotfix.Module.UI;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Game.Hotfix
 {
     
-    public class HomeViewModel : ViewModel
+    public class HomeViewModel : ViewModel,IPerloadViewModel
     {
+        int aaa = 21;
         BindableProperty<string> title = new BindableProperty<string>("Hello ILRuntime");
-        List<int> aaa = new List<int>();
+        List<int> bbb = new List<int>();
 
         public override void Init()
         {
             base.Init();
+            Debug.Log("HomeViewModelInit");
         }
     }
 }
