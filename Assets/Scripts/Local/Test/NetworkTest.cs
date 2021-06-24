e580927ebf4172555b3e50e9f8349b8528b4032d
fujisheng <fujisheng@foxmail.com>
2021-06-24 19:35:24 +0800

    save

--- Assets/Scripts/Local/Test/NetworkTest.cs
+++ Assets/Scripts/Local/Test/NetworkTest.cs
@@ -8,18 +8,19 @@ using UnityEngine;
 
 public class NetworkTest : State<Launcher>
 {
-    public string serverIp = "127.0.0.1";// "192.168.20.54";
-    public int port = 10001;// 8889;
+    public string serverIp = "192.168.20.54";
+    public int port = 8889;
     INetworkService network;
 
-    byte[] encryptSeed;
-    byte[] decryptSeed;
+    uint encryptSeed;
+    uint decryptSeed;
 
     public override void OnEnter(IFSM<Launcher> fsm)
     {
         network = Services.Get<INetworkService>();
         network.SetPackager(new DefaultPackager(new PacketPool()));
         network.SetNetworkChannel(new TcpChannel());
+        network.SetSerializer(new ProtobufSerializer());
 
         network.OnConnectionSuccessfulHandler += OnConnected;
         network.OnConnectionFailedHandler += OnConnectionFailed;
@@ -31,7 +32,6 @@ public class NetworkTest : State<Launcher>
     void OnConnected(IAsyncResult result)
     {
         Debug.Log("连接服务器成功");
-        //network.Send()
     }
 
     void OnConnectionFailed(string msg)
@@ -39,27 +39,46 @@ public class NetworkTest : State<Launcher>
         Debug.Log($"连接服务器失败 : {msg}");
     }
 
+    bool isFirst = true;
     void OnReceive(IPacket packet)
     {
-        UnityEngine.Debug.Log($"收到消息{bytesToString(packet.Bytes)}");
-        if (encryptSeed == null || decryptSeed == null)
+        if (encryptSeed == 0 || decryptSeed == 0)
         {
-            var bytes = packet.Bytes;
-            encryptSeed = GetRange(bytes, 0, 3).Reverse().ToArray();
-            decryptSeed = GetRange(bytes, 4, 7).Reverse().ToArray();
+            var bytes = packet.Data;
+            UnityEngine.Debug.Log(bytesToString(bytes, 8));
+            var encryptSeedBytes = GetRange(bytes, 0, 3).Reverse().ToArray();
+            var decryptSeedBytes = GetRange(bytes, 4, 7).Reverse().ToArray();
+            encryptSeed = BitConverter.ToUInt32(encryptSeedBytes, 0);
+            decryptSeed = BitConverter.ToUInt32(decryptSeedBytes, 0);
+
+            UnityEngine.Debug.Log($"收到的加密种子 encrypt:{encryptSeed}  decrypt:{decryptSeed}");
             network.SetEncryptor(new DefaultEncryptor(encryptSeed, decryptSeed));
+            isFirst = false;
         }
-        Debug.Log($"id:{packet.Id}");
-        Debug.Log($"Length:{packet.Length}");
-        
-        network.Send(0, new Role.req_create
+
+        if(!isFirst)// && packet.Head.ID == (3<<8 | 1))
         {
-            name = "sssss"
-        });
-        //network.Send<LoginBySessionC2S>(0, new LoginBySessionC2S
-        //{
-        //    Id = 10001
-        //});
+            var s = new ProtobufSerializer();
+            var r = s.Deserialize<GamerPVPPingS2C>(packet.Data);
+            UnityEngine.Debug.Log($"serverTime:{r.serverTime}" );
+        }
+    }
+
+    float time = 0;
+    public override void OnUpdate(IFSM<Launcher> fsm)
+    {
+        time += Time.deltaTime;
+        if(time > 2f)
+        {
+            UnityEngine.Debug.Log("发了一个心跳包");
+            ushort id = 3 << 8 | 1;
+
+            network.Send<GamerPVPPingC2S>(id, new GamerPVPPingC2S
+            {
+                clientTime = 101010,
+            });
+            time = 0;
+        }
     }
 
     byte[] GetRange(byte[] bytes, int startIndex, int endIndex)
@@ -68,18 +87,19 @@ public class NetworkTest : State<Launcher>
         int j = 0;
         for (int i = startIndex; i <= endIndex; i++, j++)
         {
-            Debug.Log($"i:{i} j:{j}");
             result[j] = bytes[i];
         }
         return result;
     }
 
-    public static string bytesToString(byte[] bytes)
+    public static string bytesToString(byte[] bytes, int length)
     {
         var result = "";
-        foreach (var b in bytes)
+        for(int i = 0; i < length; i++)
         {
+            var b = bytes[i];
             result += b;
+            result += " ";
         }
         return result;
     }
